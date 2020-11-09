e14c455fab8036b4060f2eb9bef13e7406cb5312
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-11-09 16:48:13 +0700

    change favicon and recolor title

--- resources/views/layouts/_partials/header.blade.php
+++ resources/views/layouts/_partials/header.blade.php
@@ -1,7 +1,7 @@
 <div class="navigation-bar">
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
         <div class="container">
-            <a class="navbar-brand" href="/"><b>Beeflix</b></a>
+            <a class="navbar-brand text-danger" href="/"><b>Beeflix</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
