ce3be6e22549b6fe473c0634c6f762d192cce3bf
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2020-11-10 22:32:43 +0700

    change bootstrap dir

--- resources/views/layouts/_partials/script.blade.php
+++ resources/views/layouts/_partials/script.blade.php
@@ -1,4 +1,4 @@
 <script language="JavaScript" type="text/javascript" src="{{asset('/js/jquery.js')}}"></script>
-<script language="JavaScript" type="text/javascript" src="{{asset('/bootstrap-4.4.1-dist/js/bootstrap.min.js')}}"></script>
+<script language="JavaScript" type="text/javascript" src="{{asset('/bootstrap-4.4.1/js/bootstrap.min.js')}}"></script>
 @stack('scripts')
 @stack('inline-scripts')
--- resources/views/layouts/_partials/style.blade.php
+++ resources/views/layouts/_partials/style.blade.php
@@ -3,9 +3,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="{{asset('/bootstrap-4.4.1-dist/css/bootstrap.min.css')}}" />
-<link rel="stylesheet" href="{{asset('/bootstrap-4.4.1-dist/css/bootstrap-grid.min.css')}}" />
-<link rel="stylesheet" href="{{asset('/bootstrap-4.4.1-dist/css/bootstrap-reboot.min.css')}}" />
+<link rel="stylesheet" href="{{asset('/bootstrap-4.4.1/css/bootstrap.min.css')}}" />
+<link rel="stylesheet" href="{{asset('/bootstrap-4.4.1/css/bootstrap-grid.min.css')}}" />
+<link rel="stylesheet" href="{{asset('/bootstrap-4.4.1/css/bootstrap-reboot.min.css')}}" />
 
 <!-- Main css -->
 <link rel="stylesheet" href="{{asset('/css/style.css')}}?key=<?php echo date('d-M-y'); ?>" />
